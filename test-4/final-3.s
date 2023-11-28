941fae3e9c5768fde67552ddf5a2d0a8da15f63b
PKrystian <krystian.pinczak2001@gmail.com>
2023-11-28 15:53:35 +0100

    final-3 fix

--- test-4/final-3.s
+++ test-4/final-3.s
@@ -3,30 +3,28 @@
     # ------
     # arrays
     # ------
-    # I would ignore this task in the exam.
 .text
-_start:
-    mov $6, %EAX
-    mov $7, %EBX
-    mov $8, %ECX
-    mov $9, %EDX
-__begin:
-    proc: sub %ESP, %EAX
-        mov 4(%ESP), %ECX
-        mov 8(%ESP), %EDX
-        mov %ESP, %EBX
-        ret
-    _start2: push %EAX
-        mov %ESP, %EAX
-        push %EBX
-        call proc
-        sub %ESP, %EBX
+mov $6, %EAX
+mov $7, %EBX
+mov $8, %ECX
+mov $9, %EDX
+call proc
+proc: sub %ESP, %EAX
+    mov 4(%ESP), %ECX
+    mov 8(%ESP), %EDX
+    mov %ESP, %EBX
+    ret
+_start: push %EAX
+    mov %ESP, %EAX
+    push %EBX
+    call proc
+    sub %ESP, %EBX
 __end: nop
 
 #+-----+-------+---------+
 #| reg | input |  output |
 #+-----+-------+---------+
-#| EAX |   6   |  14102  |
-#| EBX |   7   | -14096  |
-#| ECX |   8   | -13515  |
+#| EAX |   6   |    8    |
+#| EBX |   7   |   -4    |
+#| ECX |   8   |    0    |
 #| EDX |   9   |    0    |
